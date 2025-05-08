de431e012897a797ca95d7a6f961dd09595c9257
Xackery <xackery@gmail.com>
2025-05-08 16:45:45 +0000

    Fix sync to use proper paths

--- sync.bat
+++ sync.bat
@@ -1,3 +1,3 @@
-scp g16:/src/rebuildeq/server/base/expansion/_patch/dbstr_us.txt rof\
-scp g16:/src/rebuildeq/server/base/expansion/_patch/spells_us.txt rof\
+scp g16:/src/rebuildeq/server/base/_patch/dbstr_us.txt rof\
+scp g16:/src/rebuildeq/server/base/_patch/spells_us.txt rof\
 scp g16:/src/rebuildeq/server/art/bin/* rof\uifiles\default
